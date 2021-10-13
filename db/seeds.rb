55b66c4784dae68e4877b7e8be49ef76b3ab7a7a
Pedro Bernardes <phec06@gmail.com>
2021-10-12 22:23:36 -0300

    Pqp vei

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ user.update(email: 'phec06@gmail.com', name: 'Pedro')
 consolidate_wallets = UserStocks::ConsolidateWallets.new(user: user)
 consolidate_wallets.duct_tapped_ensure_user_stock_wallets
 
+
 # Seeding
 # puts "Stock Kpis..."
 # Seeders::StockKpis.new.seed
