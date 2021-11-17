88d3f0f1ea7d6e0ee599fd2841c03d223bfdb704
Pedro Bernardes <phec06@gmail.com>
2021-11-16 21:26:23 -0300

    (Probably) less annoying Luna

--- app/jobs/watched_prices_notifications_job.rb
+++ app/jobs/watched_prices_notifications_job.rb
@@ -1,24 +1,86 @@
+class Message
+  attr_reader :id, :text
+
+  def initialize(id, text)
+    @id = id
+    @text = text
+  end
+end
+
 class BuyAtPrice
-  def initialize(stock_code, target_price)
+  attr_reader :stock_code, :current_price
+
+  def initialize(stock_code:, target_price:, last_status: nil, last_price: nil)
     @stock_code = stock_code
     @target_price = target_price.to_f
+    @last_status = last_status
+    @last_price = last_price
   end
 
   def message(prices)
-    current_price = prices['currentPrice'].to_f
+    @current_price = prices['currentPrice'].to_f
+
+    return nil if current_price.zero?
 
     if current_price <= @target_price
-      # return Message.new("buy-at-price-hit-#{@stock_code}", )
-      return "#{@stock_code}. Alvo atingido: #{'%.2f' % @target_price}"
+      @status = 'target'
+      return nil if @last_status == 'target'
+
+      return "#{@stock_code} no alvo: #{'%.2f' % @target_price} (#{'%.2f' % current_price})"
     end
 
-    if current_price <= (@target_price * 1.015)
+    if @current_price <= (@target_price * 1.01)
+      @status = 'close_to_target'
+
+      return nil if @last_status == 'close_to_target' && current_price >= @last_price
+
       return "#{@stock_code}. Em zona de compra: #{'%.2f' % current_price}, alvo: #{'%.2f' % @target_price}"
     end
   end
 
   def description
     "#{@stock_code} --- #{'%.2f' % @target_price}"
+  end
+
+  def id
+    "#{@stock_code}-#{@target_price}"
+  end
+
+  def attributes
+    {
+      stock_code: @stock_code,
+      target_price: @target_price,
+      last_status: @status,
+      last_price: @current_price
+    }
+  end
+end
+
+class Alerts
+  def initialize(alerts_attributes)
+    @by_code = {}
+    @by_id = {}
+
+    alerts_attributes.each do |alert_attributes|
+      alert = BuyAtPrice.new( ** alert_attributes.deep_symbolize_keys)
+      add alert
+    end
+  end
+
+  def add(alert)
+    return if @by_id[alert.id]
+
+    @by_code[alert.stock_code] ||= []
+    @by_code[alert.stock_code].push alert
+    @by_id[alert.id] = alert
+  end
+
+  def by_code(code)
+    @by_code[code]
+  end
+
+  def serialize
+    @by_id.values.map(&:attributes).to_json
   end
 end
 
@@ -27,13 +89,18 @@ class WatchedPricesNotificationsJob < ApplicationJob
 
   def alerts
     unless @alerts
-      @alerts = {}
+      alerts_attributes = begin
+                            JSON.parse(redis.get("WATCHED_PRICES_ALERTS_ATTRIBUTES"))
+                          rescue
+                            []
+                          end
+
+      @alerts = Alerts.new alerts_attributes
 
       File.read(Rails.root.join('alerts.csv').to_s).split("\n").each do |line|
-        code, type, target_price = line.split ';'
+        stock_code, type, target_price = line.split ';'
 
-        @alerts[code] ||= []
-        @alerts[code].push BuyAtPrice.new code, target_price
+        @alerts.add BuyAtPrice.new stock_code: stock_code, target_price: target_price
       end
     end
 
@@ -74,20 +141,12 @@ class WatchedPricesNotificationsJob < ApplicationJob
 
     # TODO:
     # ENBR3 Just signaled top of channel reversal.
-
-
-    luna_messages = begin
-                      JSON.parse(redis.get("LUNA_ALERT_MESSAGES") || '{}').map do |message, at|
-                        [message, Time.parse(at)]
-                      end.to_h
-                    rescue
-                      {}
-                    end
+    redis.del "LUNA_ALERT_MESSAGES"
 
     new_messages = []
 
     price_updates.each do |stock_code, prices|
-      stock_alerts = alerts[stock_code]
+      stock_alerts = alerts.by_code stock_code
 
       next unless stock_alerts
 
@@ -97,17 +156,8 @@ class WatchedPricesNotificationsJob < ApplicationJob
       end
     end
 
-    to_be_sent = []
-
-    new_messages.each do |message|
-      if !luna_messages[message] || luna_messages[message] < 5.minutes.ago
-        to_be_sent.push message
-        luna_messages[message] = Time.now.to_s
-      end
-    end
-
-    final_message = to_be_sent.join("\n")
-    redis.set "LUNA_ALERT_MESSAGES", luna_messages.to_json
+    final_message = new_messages.join("\n")
+    redis.set "WATCHED_PRICES_ALERTS_ATTRIBUTES", alerts.serialize
 
     luna.message final_message unless final_message.blank?
     ensure_good_morning
